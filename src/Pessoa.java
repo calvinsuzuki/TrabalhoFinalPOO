cb0bc38ac2807868509f0e973ccfd671752bb321
calvinsuzuki <calvinssdcamargo@gmail.com>
2021-07-09 19:57:31 -0300

    Project Creation

---
+++ src/Pessoa.java
@@ -0,0 +1,14 @@
+
+public class Pessoa {
+	
+	long register;
+	String nome;
+	float freq;
+	
+	Pessoa(long _register, String _nome, float _freq) {
+		register = _register;
+		nome = _nome;
+		freq = _freq;
+	}
+
+}
