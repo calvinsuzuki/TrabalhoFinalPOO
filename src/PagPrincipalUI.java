027216667158e96cc6e2d6efcf95721849d347e6
BatsBatsQrz <g.batistella@yahoo.com.br>
2021-07-13 20:36:09 -0300

    added navegacao pelas abas da pagina principal
    botei os botoes de ir pra primeira, ultima, proxima e anterior, botei mais um monte de aluno pra testar

--- src/PagPrincipalUI.java
+++ src/PagPrincipalUI.java
@@ -4,7 +4,6 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingConstants;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
@@ -14,6 +13,8 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.MatteBorder;
@@ -30,13 +31,18 @@ public class PagPrincipalUI extends JFrame {
 	private JPanel contentPane;
 	private JPanel titlePane;
 	private JPanel cardsPane;
+	private JPanel[] topDownPanes;
 	private JPanel[] listPanes;
+	private JPanel[] previousNextPanes;
 	private JScrollPane[] scrollPanes;
-	private JLabel lblAddPessoa;
 	private JTextField txtBusca;
 	private JButton btnBusca;
 	private JButton[] btnsPessoa;
 	private JButton btnAddPessoa;
+	private JButton[] btnsPrevious;
+	private JButton[] btnsNext;
+	private JButton[] btnsFirst;
+	private JButton[] btnsLast;
 
 	/**
 	 * Launch the application.
@@ -157,28 +163,64 @@ public class PagPrincipalUI extends JFrame {
 	}
 	
 	private void refreshListPessoas(Escola sistema, Pessoa contaLogada) {
-		cardsPane.removeAll();
-		cardsPane.repaint();
-		cardsPane.revalidate();
-		
 		int numPessoas = sistema.getNPessoas();
 		int numPaginas = numPessoas/27 + 1;
 		int numPessoasUltimaPagina = numPessoas - (numPaginas-1)*27;
 		ArrayList<Pessoa> pessoas = sistema.getPessoas();
 		String[] dadosPessoas = sistema.imprimePessoas();
 		
+		refreshListPessoas(numPessoas, numPaginas, numPessoasUltimaPagina, dadosPessoas, pessoas, contaLogada);
+	}
+	
+	private void refreshListPessoas(ArrayList<Pessoa> pessoasParaMostrar, Pessoa contaLogada) {
+		int numPessoas = pessoasParaMostrar.size();
+		int numPaginas = numPessoas/27 + 1;
+		int numPessoasUltimaPagina = numPessoas - (numPaginas-1)*27;
+		String[] dadosPessoas = new String[numPessoas];
+		for(int i=0 ; i<numPessoas ; i++) {
+			dadosPessoas[i] = pessoasParaMostrar.get(i).getClass().toString() + "\nNome: " + pessoasParaMostrar.get(i).getNome() + "\nNº de Registro: " + pessoasParaMostrar.get(i).getRegister();
+		}
+		
+		refreshListPessoas(numPessoas, numPaginas, numPessoasUltimaPagina, dadosPessoas, pessoasParaMostrar, contaLogada);
+	}
+	
+	private void refreshListPessoas(int numPessoas, int numPaginas, int numPessoasUltimaPagina, String[] dadosPessoas, ArrayList<Pessoa> pessoas, Pessoa contaLogada) {
+		cardsPane.removeAll();
+		cardsPane.repaint();
+		cardsPane.revalidate();
+		
+		topDownPanes = new JPanel[numPaginas];
 		listPanes = new JPanel[numPaginas];
+		previousNextPanes = new JPanel[numPaginas];
 		btnsPessoa = new JButton[numPessoas];
+		btnsPrevious = new JButton[numPaginas];
+		btnsNext = new JButton[numPaginas];
+		btnsFirst = new JButton[numPaginas];
+		btnsLast = new JButton[numPaginas];
 		scrollPanes = new JScrollPane[numPaginas];
 		
 		for(int i=0 ; i<numPaginas ; i++) {
+			topDownPanes[i] = new JPanel();
+			topDownPanes[i].setForeground(new Color(0, 0, 0));
+			topDownPanes[i].setBackground(new Color(240, 230, 140));
+			topDownPanes[i].setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(218, 165, 32)));
+			topDownPanes[i].setPreferredSize(new Dimension(1310, 1400));
+			topDownPanes[i].setLayout(new BorderLayout());
+			topDownPanes[i].setAutoscrolls(true);
+			
 			listPanes[i] = new JPanel();
 			listPanes[i].setForeground(new Color(0, 0, 0));
 			listPanes[i].setBackground(new Color(240, 230, 140));
-			listPanes[i].setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(218, 165, 32)));
-			listPanes[i].setPreferredSize(new Dimension(1310, 1500));
+			listPanes[i].setBorder(new EmptyBorder(0, 0, 0, 0));
+			listPanes[i].setPreferredSize(new Dimension(1310, 1200));
 			listPanes[i].setLayout(new FlowLayout(FlowLayout.LEFT, 26, 26));
-			listPanes[i].setAutoscrolls(true);
+			
+			previousNextPanes[i] = new JPanel();
+			previousNextPanes[i].setForeground(new Color(0, 0, 0));
+			previousNextPanes[i].setBackground(new Color(240, 230, 140));
+			previousNextPanes[i].setBorder(new EmptyBorder(0, 0, 0, 0));
+			previousNextPanes[i].setPreferredSize(new Dimension(1310, 200));
+			previousNextPanes[i].setLayout(new GridLayout(3, 2, 815, 20));
 			
 			int numPessoasNessaPagina;
 			if(i == numPaginas-1) {
@@ -189,7 +231,7 @@ public class PagPrincipalUI extends JFrame {
 			
 			for(int j=0 ; j<numPessoasNessaPagina ; j++) {
 				int pessoaIndex = 27*i + j;
-				String infoFormatada = "<html>" + dadosPessoas[pessoaIndex].replaceAll("\\n", "<br/>") + "</html>";
+				String infoFormatada = "<html>" + dadosPessoas[pessoaIndex].substring(6).replaceAll("\\n", "<br/>") + "</html>";
 				btnsPessoa[pessoaIndex] = new JButton(infoFormatada);
 				btnsPessoa[pessoaIndex].setHorizontalAlignment(SwingConstants.LEFT);
 				btnsPessoa[pessoaIndex].setFont(new Font("Papyrus", Font.BOLD, 18));
@@ -216,9 +258,10 @@ public class PagPrincipalUI extends JFrame {
 			}
 			
 			if(i == numPaginas-1 && contaLogada instanceof Diretor) {
-				btnAddPessoa = new JButton();
+				btnAddPessoa = new JButton("<html>Adicionar<br/>nova Pessoa</html>");
+				btnAddPessoa.setFont(new Font("Papyrus", Font.BOLD | Font.ITALIC, 22));
 				btnAddPessoa.setIcon(new ImageIcon(new ImageIcon(".\\UI Icons\\plus.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
-				btnAddPessoa.setPreferredSize(new Dimension(60, 60));
+				btnAddPessoa.setPreferredSize(new Dimension(220, 80));
 				btnAddPessoa.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent evt) {
 						EventQueue.invokeLater(new Runnable() {
@@ -234,16 +277,32 @@ public class PagPrincipalUI extends JFrame {
 					}
 				});
 				
-				lblAddPessoa = new JLabel("<html>Adicionar<br/>nova Pessoa</html>");
-				lblAddPessoa.setForeground(new Color(140, 140, 140));
-				lblAddPessoa.setFont(new Font("Papyrus", Font.BOLD | Font.ITALIC, 30));
-				lblAddPessoa.setPreferredSize(new Dimension(240, 100));
-				
 				listPanes[i].add(btnAddPessoa);
-				listPanes[i].add(lblAddPessoa);
 			}
 			
-			scrollPanes[i] = new JScrollPane(listPanes[i]);
+			if(numPaginas > 1) {
+				if(i == 0) {
+					addButtonPrevious(i, false);
+					addButtonNext(i, true);
+					addButtonFirst(i, false);
+					addButtonLast(i, true);
+				} else if(i == numPaginas-1) {
+					addButtonPrevious(i, true);
+					addButtonNext(i, false);
+					addButtonFirst(i, true);
+					addButtonLast(i, false);
+				} else {
+					addButtonPrevious(i, true);
+					addButtonNext(i, true);
+					addButtonFirst(i, true);
+					addButtonLast(i, true);
+				}
+			}
+			
+			topDownPanes[i].add(listPanes[i], BorderLayout.NORTH);
+			topDownPanes[i].add(previousNextPanes[i], BorderLayout.SOUTH);
+			
+			scrollPanes[i] = new JScrollPane(topDownPanes[i]);
 			scrollPanes[i].setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
 			scrollPanes[i].setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 			scrollPanes[i].setPreferredSize(new Dimension(1330, 628));
@@ -252,103 +311,72 @@ public class PagPrincipalUI extends JFrame {
 		}
 	}
 	
-	private void refreshListPessoas(ArrayList<Pessoa> pessoasParaMostrar, Pessoa contaLogada) {
-		cardsPane.removeAll();
-		cardsPane.repaint();
-		cardsPane.revalidate();
-		
-		int numPessoas = pessoasParaMostrar.size();
-		System.out.println(numPessoas);
-		int numPaginas = numPessoas/27 + 1;
-		int numPessoasUltimaPagina = numPessoas - (numPaginas-1)*27;
-		String[] dadosPessoas = new String[numPessoas];
-		for(int i=0 ; i<numPessoas ; i++) {
-			dadosPessoas[i] = pessoasParaMostrar.get(i).getClass().toString() + "\nNome: " + pessoasParaMostrar.get(i).getNome() + "\nNº de Registro: " + pessoasParaMostrar.get(i).getRegister();
+	private void addButtonPrevious(int i, boolean verdade) {
+		if(!verdade) {
+			btnsPrevious[i] = null;
+			previousNextPanes[i].add(new JPanel());
+			return;
 		}
-		
-		listPanes = new JPanel[numPaginas];
-		btnsPessoa = new JButton[numPessoas];
-		scrollPanes = new JScrollPane[numPaginas];
-		
-		for(int i=0 ; i<numPaginas ; i++) {
-			listPanes[i] = new JPanel();
-			listPanes[i].setForeground(new Color(0, 0, 0));
-			listPanes[i].setBackground(new Color(240, 230, 140));
-			listPanes[i].setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(218, 165, 32)));
-			listPanes[i].setPreferredSize(new Dimension(1310, 1500));
-			listPanes[i].setLayout(new FlowLayout(FlowLayout.LEFT, 26, 26));
-			listPanes[i].setAutoscrolls(true);
-			
-			int numPessoasNessaPagina;
-			if(i == numPaginas-1) {
-				numPessoasNessaPagina = numPessoasUltimaPagina;
-			} else {
-				numPessoasNessaPagina = 27;
+		btnsPrevious[i] = new JButton("Página Anterior");
+		btnsPrevious[i].setFont(new Font("Papyrus", Font.BOLD | Font.ITALIC, 21));
+		btnsPrevious[i].setIcon(new ImageIcon(new ImageIcon(".\\UI Icons\\previousArrow.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
+		btnsPrevious[i].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				((CardLayout)cardsPane.getLayout()).previous(cardsPane);
 			}
-			
-			for(int j=0 ; j<numPessoasNessaPagina ; j++) {
-				int pessoaIndex = 27*i + j;
-				String infoFormatada = "<html>" + dadosPessoas[pessoaIndex].replaceAll("\\n", "<br/>") + "</html>";
-				btnsPessoa[pessoaIndex] = new JButton(infoFormatada);
-				btnsPessoa[pessoaIndex].setHorizontalAlignment(SwingConstants.LEFT);
-				btnsPessoa[pessoaIndex].setFont(new Font("Papyrus", Font.BOLD, 18));
-				btnsPessoa[pessoaIndex].setPreferredSize(new Dimension(400, 100));
-				/*btnsPessoa[pessoaIndex].addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent evt) {
-						if(checkPermission(contaLogada, pessoasParaMostrar.get(pessoaIndex))) {
-							EventQueue.invokeLater(new Runnable() {
-								public void run() {
-									try {
-										InfoPessoaUI frame = new InfoPessoaUI(pessoasParaMostrar.get(pessoaIndex), contaLogada);
-										frame.setVisible(true);
-									} catch (Exception e) {
-										e.printStackTrace();
-									}
-								}
-							});
-						} else {
-							JOptionPane.showMessageDialog(null, "Você não tem permissão para ver essas informações!", "ACESSO NEGADO", JOptionPane.ERROR_MESSAGE);
-						}
-					}
-				});*/
-				listPanes[i].add(btnsPessoa[27*i + j]);
-			}
-			
-			if(i == numPaginas-1 && contaLogada instanceof Diretor) {
-				btnAddPessoa = new JButton();
-				btnAddPessoa.setIcon(new ImageIcon(new ImageIcon(".\\UI Icons\\plus.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
-				btnAddPessoa.setPreferredSize(new Dimension(60, 60));
-				btnAddPessoa.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent evt) {
-						EventQueue.invokeLater(new Runnable() {
-							public void run() {
-								try {
-									AddPessoaUI frame = new AddPessoaUI();
-									frame.setVisible(true);
-								} catch (Exception e) {
-									e.printStackTrace();
-								}
-							}
-						});
-					}
-				});
-				
-				lblAddPessoa = new JLabel("<html>Adicionar<br/>nova Pessoa</html>");
-				lblAddPessoa.setForeground(new Color(140, 140, 140));
-				lblAddPessoa.setFont(new Font("Papyrus", Font.BOLD | Font.ITALIC, 30));
-				lblAddPessoa.setPreferredSize(new Dimension(240, 100));
-				
-				listPanes[i].add(btnAddPessoa);
-				listPanes[i].add(lblAddPessoa);
-			}
-			
-			scrollPanes[i] = new JScrollPane(listPanes[i]);
-			scrollPanes[i].setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
-			scrollPanes[i].setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-			scrollPanes[i].setPreferredSize(new Dimension(1330, 628));
-			
-			cardsPane.add(scrollPanes[i]);
+		});
+		previousNextPanes[i].add(btnsPrevious[i]);
+	}
+	
+	private void addButtonNext(int i, boolean verdade) {
+		if(!verdade) {
+			btnsNext[i] = null;
+			previousNextPanes[i].add(new JPanel());
+			return;
 		}
+		btnsNext[i] = new JButton("Próxima Página");
+		btnsNext[i].setFont(new Font("Papyrus", Font.BOLD | Font.ITALIC, 21));
+		btnsNext[i].setIcon(new ImageIcon(new ImageIcon(".\\UI Icons\\nextArrow.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
+		btnsNext[i].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				((CardLayout)cardsPane.getLayout()).next(cardsPane);
+			}
+		});
+		previousNextPanes[i].add(btnsNext[i]);
+	}
+	
+	private void addButtonFirst(int i, boolean verdade) {
+		if(!verdade) {
+			btnsFirst[i] = null;
+			previousNextPanes[i].add(new JPanel());
+			return;
+		}
+		btnsFirst[i] = new JButton("Primeira Página");
+		btnsFirst[i].setFont(new Font("Papyrus", Font.BOLD | Font.ITALIC, 21));
+		btnsFirst[i].setIcon(new ImageIcon(new ImageIcon(".\\UI Icons\\firstArrow.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
+		btnsFirst[i].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				((CardLayout)cardsPane.getLayout()).first(cardsPane);
+			}
+		});
+		previousNextPanes[i].add(btnsFirst[i]);
+	}
+	
+	private void addButtonLast(int i, boolean verdade) {
+		if(!verdade) {
+			btnsLast[i] = null;
+			previousNextPanes[i].add(new JPanel());
+			return;
+		}
+		btnsLast[i] = new JButton("Última Página");
+		btnsLast[i].setFont(new Font("Papyrus", Font.BOLD | Font.ITALIC, 21));
+		btnsLast[i].setIcon(new ImageIcon(new ImageIcon(".\\UI Icons\\lastArrow.png").getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
+		btnsLast[i].addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				((CardLayout)cardsPane.getLayout()).last(cardsPane);
+			}
+		});
+		previousNextPanes[i].add(btnsLast[i]);
 	}
 	
 	private boolean checkPermission(Pessoa contaLogada, Pessoa aSerVista) {
