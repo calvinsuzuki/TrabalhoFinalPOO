68d5307e35d630fb4e3fc8bb82d95f840be45d03
PedroRaymundi <62019741+PedroRaymundi@users.noreply.github.com>
2021-07-25 19:26:08 -0300

    Add files via upload

--- src/GerenciadorDados.java
+++ src/GerenciadorDados.java
@@ -3,7 +3,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 import exceptions.RegistroUsadoException;
